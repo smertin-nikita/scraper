adb1c49388341ec6907ba02fc5a8be4e42e27259
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-02 13:38:20 +0500

    Gets articles by KEYWORDS

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
 
 if __name__ == '__main__':
-    pass
+    pattern = re.compile('|'.join(KEYWORDS), re.IGNORECASE)
+
+    response = requests.get(
+        url='https://habr.com/ru/all/'
+        )
+    response.raise_for_status()
+
+    text = response.text
+
+    soup = BeautifulSoup(text, features='html.parser')
+    articles = soup.find_all('article', class_='post post_preview')
+    for article in articles:
+        if pattern.search(article.text):
+            time = article.find('span', class_='post__time').text
+            title = article.find('a', class_='post__title_link')
+            print(time, title.text, title.get('href'))
 
