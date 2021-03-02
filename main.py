1e40366fdcffdfdea81ce91a3ccb060729d01d83
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-02 14:20:57 +0500

    Searching in every article's text

--- main.py
+++ main.py
@@ -18,9 +18,20 @@ if __name__ == '__main__':
 
     soup = BeautifulSoup(text, features='html.parser')
     articles = soup.find_all('article', class_='post post_preview')
-    for article in articles:
+    urls = [article.find('a', class_='post__habracut-btn').get('href') for article in articles]
+
+    for url in urls:
+        response = requests.get(
+            url=url
+            )
+        response.raise_for_status()
+
+        text = response.text
+        bs = BeautifulSoup(text, features='html.parser')
+        article = bs.find('article', class_='post post_full')
+
         if pattern.search(article.text):
-            time = article.find('span', class_='post__time').text
-            title = article.find('a', class_='post__title_link')
-            print(time, title.text, title.get('href'))
+            time = article.find('span', class_='post__time').get('data-time_published')
+            title = article.find('span', class_='post__title-text').text
+            print(time, title, url)
 
